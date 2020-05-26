a192d2736c34f0e2940a8332a21470d975674d0f
Luis Christina <luisfelipeagarcia@gmail.com>
2020-05-26 17:52:21 -0300

    cli insert

---
+++ DeMobile/cli_inserir.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DeMobile
+{
+    public partial class cli_inserir : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
