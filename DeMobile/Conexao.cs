0d69fcc6a71578077a453e1b3f166a69c2d3c0a4
Luis Christina <luisfelipeagarcia@gmail.com>
2020-05-26 17:38:33 -0300

    Conexao

--- DeMobile/Conexao.cs
+++ DeMobile/Conexao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,35 @@ namespace DeMobile
 {
     public class Conexao
     {
+        private static string Server = "localhost";
+        private static string Database = "web_DeMobile";
+        private static string User = "root";
+        private static string Password = "123456";
+
+        private static string connectionString = $@"Server={Server};
+                                                    Database={Database};
+                                                    Uid={User};
+                                                    Pwd={Password};
+                                                    SslMode=none;charset=utf8;";
+
+
+        public static MySqlConnection Connection = new MySqlConnection(connectionString);
+
+        public static void Conectar()
+        {
+            if (Connection.State == System.Data.ConnectionState.Closed)
+            {
+                Connection.Open();
+            }
+        }
+
+        public static void Desconectar()
+        {
+            if (Connection.State == System.Data.ConnectionState.Open)
+            {
+                Connection.Close();
+            }
+        }
+
     }
 }
